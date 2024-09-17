5797cdba7d50900b04763463d99be7dd6b47cfde
pepedinho <2spii94@gmail.com>
2024-09-17 22:55:38 +0200

    fix unused import and variables

--- src/listener/listening.rs
+++ src/listener/listening.rs
@@ -1,14 +1,6 @@
-use crossterm::event::{self, KeyCode, KeyEvent};
-use std::{
-    io::{self},
-    time::Duration,
-    usize,
-};
-
 pub struct Buffer {
     pub line: String,
     pub index: usize,
-    pub is_selected: bool,
     pub is_completed: bool,
     pub is_dir: bool,
     pub is_deploy: bool,
@@ -16,22 +8,18 @@ pub struct Buffer {
 
 pub struct Task {
     pub buffer: Vec<Buffer>,
-    pub name: String,
-    pub s_index: usize,
 }
 
 impl Buffer {
     pub fn new() -> Self {
         let line = String::new();
         let index = 0;
-        let is_selected = false;
         let is_completed = false;
         let is_dir = false;
         let is_deploy = true;
         Buffer {
             line,
             index,
-            is_selected,
             is_completed,
             is_dir,
             is_deploy,
@@ -42,12 +30,6 @@ impl Buffer {
 impl Task {
     pub fn new() -> Self {
         let buffer = Vec::new();
-        let s_index = 0;
-        let name = String::new();
-        Task {
-            buffer,
-            s_index,
-            name,
-        }
+        Task { buffer }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use file_system::file::TaskBuf;
-use listener::listening::Task;
 
 mod display;
 mod file_system;
@@ -9,9 +8,4 @@ mod save_task;
 fn main() {
     let mut task = TaskBuf::new();
     task.listen().ok();
-    //task.tasks[0] = Task::new();
-    //task.tasks[0].file_to_task().ok();
-    //task.file_to_task().ok();
-    //task.listen().ok();
-    //task.save_task().ok();
 }
